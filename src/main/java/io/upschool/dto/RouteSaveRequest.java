bceab588ed507526f2a61514dc946495e9a8671d
nurrbb <enurb320@gmail.com>
2025-12-18 17:24:33 +0300

    feat: add duration constraints to route requests

--- src/main/java/io/upschool/dto/RouteSaveRequest.java
+++ src/main/java/io/upschool/dto/RouteSaveRequest.java
@@ -1,5 +1,6 @@
 package io.upschool.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Data
@@ -8,8 +9,15 @@ import lombok.*;
 @Builder
 public class RouteSaveRequest {
 
+    @NotNull(message = "Origin airport ID cannot be null")
     private Long originAirportId;
+
+    @NotNull(message = "Destination airport ID cannot be null")
     private Long destinationAirportId;
+
+    @Min(value = 1, message = "Average duration must be at least 1 minute")
+    @Max(value = 1440, message = "Average duration cannot exceed 1440 minutes (24 hours)")
+    private Integer averageDurationMinutes;
 
 }
 
